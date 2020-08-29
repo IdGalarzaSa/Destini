6e07b01897d18026bfd4660ebc497090c390496b
Ivan Galarza <id.galarza.sa@gmail.com>
2020-08-29 12:58:53 -0400

    App logic and UI

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet var storyLabel: UILabel!
+  @IBOutlet var choice1Button: UIButton!
+  @IBOutlet var choice2Button: UIButton!
+  
+  var storyBrain = StoryBrain()
+  var story:Story!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    updateUi()
+  }
+
+  @IBAction func chooiceMade(_ sender: UIButton) {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    if sender.currentTitle == story.choice1 {
+      storyBrain.setNextStory(story.choice1Destination)
+    } else {
+      storyBrain.setNextStory(story.choice2Destination)
     }
-
-
+    
+    updateUi()
+  }
+  
+  func updateUi(){
+    story = storyBrain.getCurrentStory()
+    storyLabel.text = story.title
+    choice1Button.setTitle(story.choice1, for: .normal)
+    choice2Button.setTitle(story.choice2, for: .normal)
+  }
+  
 }
 
